d2f6e539ca593f033979385e7fcd7b234e415588
muratbaevrasul7-boop <muratbaevrasul7@gmail.com>
2025-12-14 10:17:59 +0500

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,34 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+
+TOKEN = "8572236279:AAFO1GNsYJFOcFI5qv1jcSDpOSQ4nSYDa6A"
+USTOZ_CHAT_ID = 183827140
+
+async def handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.message.from_user
+
+    text = (
+        f"👤 Student: {user.full_name}\n"
+        f"🔗 Username: @{user.username}\n"
+    )
+
+    await context.bot.send_message(
+        chat_id=USTOZ_CHAT_ID,
+        text=text
+    )
+
+    if update.message.document:
+        await context.bot.send_document(
+            chat_id=USTOZ_CHAT_ID,
+            document=update.message.document.file_id
+        )
+
+    if update.message.photo:
+        await context.bot.send_photo(
+            chat_id=USTOZ_CHAT_ID,
+            photo=update.message.photo[-1].file_id
+        )
+
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(MessageHandler(filters.ALL, handler))
+app.run_polling()
